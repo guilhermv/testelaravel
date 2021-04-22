175c54e3d2321a731c58edecc746cfa0acb90e15
unknown <guiviego@gmail.com>
2021-04-22 13:13:09 -0300

    Added REST Api

---
+++ database/migrations/2021_04_21_204240_create_model_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_user')->unsigned();
+            $table->string('name');
+            $table->string('title');
+            $table->integer('pages');
+            $table->double('price',10,2);
+            $table->timestamps();
+        });
+        Schema::table('book', function (Blueprint $table) {
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
